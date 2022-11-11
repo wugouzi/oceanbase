970d955305093865bf54039bbfea71dda4e19d0c
wu <f_dogs@protonmail.com>
2022-11-11 15:27:13 +0800

    chore: add flame.sh

---
+++ tools/flame.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PID=`ps -ef | grep observer | grep -v grep | awk '{print $2}'`
+if [ ${#PID} -eq 0 ]
+then
+    echo "observer not running"
+    exit -1
+fi
+echo "1:"
+perf record -F 99 -g -p $PID -- sleep 50
+echo "2:"
+perf script -i perf.data &> perf.unfold
+echo "3:"
+FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
+echo "4:"
+FlameGraph/flamegraph.pl perf.folded > perf.svg
+rm -rf perf.data* perf.folded perf.unfold
